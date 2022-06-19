ce9861e9eceb97e99cb78fd69a49425547b57393
AhHyeon Lee <uiyn@naver.com>
2022-06-19 22:26:27 +0900

    Lv.1 정수 제곱근 판별

---
+++ programmers/연습문제/solution_정수제곱근.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = 0;
+    if(Math.sqrt(n) % 1 === 0 ) {
+        answer = (Math.sqrt(n)+1) * (Math.sqrt(n)+1)
+    }
+    else{
+        answer = -1;
+    }
+    return answer;
+}
