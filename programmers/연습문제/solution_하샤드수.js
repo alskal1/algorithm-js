f5bd4a7bfcb62ecf88c53d1457d2535b5b9f157d
AhHyeon Lee <uiyn@naver.com>
2022-06-27 22:43:52 +0900

    Lv.1 하샤드 수

---
+++ programmers/연습문제/solution_하샤드수.js
@@ -0,0 +1,9 @@
+function solution(x) {
+    let arr = String(x).split("");
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += Number(arr[i]);
+    }
+    if(x % sum === 0) return true;
+    else return false;
+}
