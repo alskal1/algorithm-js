2ec6dbd0dbdfe8b9d5b568a566cdb0f3e5c917bc
AhHyeon Lee <uiyn@naver.com>
2022-06-28 22:34:18 +0900

    Lv.1 소수 찾기

---
+++ programmers/연습문제/solution_소수찾기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let arr = [];
+    for(let i = 2; i <= n; i++){
+       arr[i] = i;
+    }
+    for(let i = 2; i <= n; i++){
+        if(arr[i] === 0) continue;
+        for(let j = i*2; j <= n; j += i){
+            arr[j] = 0;
+        }
+    }
+    let answer = 0;
+    for(let i = 2; i <= n; i++){
+        if(arr[i] !== 0) answer++;
+    }
+    
+    return answer;
+}
